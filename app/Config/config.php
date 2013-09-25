7f233c0f089b6ebfa658f2b4fe896caaa6141a9e
adhenawer <adhenawer@gmail.com>
2013-09-25 20:36:05 +0000

    add path repo git

--- app/Config/config.php
+++ app/Config/config.php
@@ -3,4 +3,4 @@ $config['user']['default']['senha']      = 123;
 $config['path']['svn']['repo']           = '/home/svn/repo';
 $config['path']['svn']['auth-users']     = '/home/svn';
 $config['path']['svn']['access-control'] = '/home/svn';
-$config['path']['git']                   = '';
+$config['path']['git']['repo']           = '/home/git';
